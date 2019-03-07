613a47b0847c68ba2a5825a2836f96d05210d035
sujilnt <sujilnt@gmail.com>
2019-03-07 23:58:39 +0000

    adding some changes

--- uberInterview.js
+++ uberInterview.js
@@ -1,23 +1,32 @@
-/*
-This problem was asked by Uber.
-Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
-
-For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
-
-Follow-up: what if you can't use division?
-*/
 function solution(input){
-  const finalResult=[];
-   for(let i =0, len = input.length;i<len;i++){
-     let value = 1;
-     let t = 1;
-     for(j=i+1;j<len;j++){
-       console.log("j",input[j])
-     }
-     console.log(input[i])
-   }
-   console.log(finalResult);
-   return finalResult;
+	// [2,3,6]
+	let leftValue =1;
+	let rightValue =1;
+	let finalValue =[];
+	for(let i = 0,len= input.length; i<len; i++){
+		if(i===0){
+			for(let j = i+1,lastItem= len;j<=lastItem;j++){
+				rightValue = (j=== lastItem) ? rightValue: input[j]*rightValue;
+			}
+			finalValue.push(rightValue);
+			rightValue=1;
 
+		}else{
+			for(let j = 0;j<=i;j++){
+				leftValue = (j=== i) ? leftValue : input[j]*leftValue;
+				console.log("leftvalue",leftValue,i,j)
+			}
+			for(let j = i+1,lastItem = len;j<lastItem;j++){
+				rightValue = (j=== lastItem) ? rightValue: input[j]*rightValue;
+			}
+			finalValue.push(leftValue*rightValue);
+			leftValue=1;
+			rightValue=1;
+		}
+
+	}
+	console.log(finalValue);
+	return finalValue;
 }
-console.log(solution([1, 2, 3, 4, 5]));
+solution([3,2,1]);
+solution([1, 2, 3, 4, 5]);
