784ffa87813eb3dcf1c0beda3d30e4d9360f9fc7
sujilnt <sujilnt@gmail.com>
2019-03-08 00:51:59 +0000

    adding some changes

--- uberInterview.js
+++ uberInterview.js
@@ -1,5 +1,16 @@
 function solution(input){
-	// [2,3,6]
+	// [2,3,6] =>
+	//[2,3,6,7]
+	/*
+	*  [ 2,3,6] =>
+	*  i=0,  j=1 to =3
+	*  i=1... n  ,
+	*  leftchild(multiplies all the element to the left),
+	*  rightChild(multiply all the elements to the right)
+	*  then => push to an array (the product of left child and right child )
+	*
+	*
+	* */
 	let leftValue =1;
 	let rightValue =1;
 	let finalValue =[];
@@ -14,7 +25,6 @@ function solution(input){
 		}else{
 			for(let j = 0;j<=i;j++){
 				leftValue = (j=== i) ? leftValue : input[j]*leftValue;
-				console.log("leftvalue",leftValue,i,j)
 			}
 			for(let j = i+1,lastItem = len;j<lastItem;j++){
 				rightValue = (j=== lastItem) ? rightValue: input[j]*rightValue;
@@ -25,8 +35,12 @@ function solution(input){
 		}
 
 	}
-	console.log(finalValue);
+	console.log(input,"=>",finalValue);
 	return finalValue;
 }
+
+const check = ()=>{
+
+};
 solution([3,2,1]);
 solution([1, 2, 3, 4, 5]);
